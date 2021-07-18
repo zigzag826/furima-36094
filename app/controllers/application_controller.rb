39746204d1cc5b9cf60e4e137d35d6cb89e3fd25
zigzag826 <86278987+zigzag826@users.noreply.github.com>
2021-07-19 01:45:08 +0900

    Basic認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'yuya' && password == '36094'
+    end
+  end
 end
