e39775811e46aa57ad3159681d6c23191a96f74f
zigzag826 <86278987+zigzag826@users.noreply.github.com>
2021-07-23 19:38:15 +0900

    ユーザー認証機能確認・単体テスト""

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :name
+    validates :birthday
+  end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/} do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/} do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+  validates :password, format: {with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+  has_many :items
+  has_many :orders
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+
 end
