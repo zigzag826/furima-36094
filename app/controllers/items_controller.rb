df474ca6c6edc22752b6df3ecd2d313071a9607f
zigzag826 <86278987+zigzag826@users.noreply.github.com>
2021-07-30 14:58:00 +0900

    商品情報編集機能・修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, only: [:update, :edit]
 
