c6bbe194023ec0f3329872c749c5d0bd07e0c790
zigzag826 <86278987+zigzag826@users.noreply.github.com>
2021-07-30 14:47:05 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:update, :edit]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -18,7 +20,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
@@ -27,5 +39,11 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:products, :text, :category_id, :status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
+  def move_to_index
+    redirect_to root_path unless current_user == @item.user
+  end
 end
